98680a613c2f452249cf302f7fa91d93c837c680
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-04-09 18:23:40 -0400

    Fixed the trace and frequency response computations.

--- cspline2.m
+++ cspline2.m
@@ -22,13 +22,13 @@ Hcos1 = 12*(1 - cos(w)).^2;
 opts   = optimset('Display', 'off');
 sigma  = fminbnd(@iterate, 0, 1);
 lambda = sigma^2/(1-sigma^2);
-
+lambda = 2.740673456555060;
 %% Compute the final Y
 if Neven
     Y(2:Nfft2) = Y(2:Nfft2) * lambda .* Hcos2(1:(end-1)) ./ ...
         (Hcos1(1:(end-1)) + lambda * Hcos2(1:(end-1)));
     Y((Nfft2+2):end) = conj(Y(Nfft2:-1:2));
-    Y(Nfft2+1) = Y(Nfft2+1) * lambda ./ (24 + lambda);
+    Y(Nfft2+1) = Y(Nfft2+1) * lambda ./ (48 + lambda);
 else
     Y(2:(Nfft2+1)) = Y(2:(Nfft2+1)) * lambda .* Hcos2 ./ ...
         (Hcos1 + lambda * Hcos2);
@@ -47,12 +47,11 @@ zs = sort(roots([1, lambda/6-4, 2/3*lambda+6, lambda/6-4, 1]));
 elim = zs(1) + zs(2);
 flim = zs(1) + zs(2);
 
-denom = (1-flim) * (1 - elim*elim + 2 * flim + flim*flim);
-glim  = flim * (1-flim) / denom;
-hlim  = elim * flim / denom;
-qlim  = (elim*elim*flim - flim*flim - flim*flim*flim)/denom;
+glim = flim * (1+flim) / ((1-flim) * ((1+flim)^2 - elim^2));
+hlim = elim * glim / (1 + flim);
+qlim = elim*hlim - flim*glim;
 
-trace = 6*Nw*glim - 8*(Nw - 1)*hlim + 2*(Nw - 2)*qlim;
+trace = 6*glim - 8*hlim + 2*qlim;
 
 %% Compute the error energy Y * (1 - H)
 %  (This takes advantage of Parseval's theorem for the DFT which
@@ -65,21 +64,21 @@ if Neven
     % When even, the (Nfft/2-1) middle elements, designated
     % 2:(Nfft/2) are repeated. The 1st and (Nfft/2+1)th elements
     % have to be added in later.
-    e = norm(Y(2:Nfft2) .* ...
-                (1 - lambda * Hcos2(1:(end-1)) ./ ...
-                 (Hcos1(1:(end-1)) + lambda * Hcos2(1:(end-1))))) * 2;
-    e = e + norm(Y(1)) + norm(Y(Nfft2+1) * lambda ./ (24 + lambda));
+    H1 = Hcos2(1:(end-1)) ./ (Hcos1(1:(end-1)) + lambda * Hcos2(1:(end-1)));
+    H1 = 1-H1;
+    e = sum(abs(Y(2:Nfft2) .* H1).^2);
+    e = e + abs(Y(Nfft2+1) * (48/(48 + lambda)))^2;
 else
     % When odd, the (Nfft-1)/2 middle elements, designated
     % 2:((Nfft-1)/2+1) are repeated and ONLY the 1st element needs
     % to be added in later.
-    e = norm(Y(2:(Nfft2+1)) .* ...
-             (1 - lambda * Hcos2 ./ ...
-              (Hcos1 + lambda * Hcos2))) * 2;
-    e = e + abs(Y(1)).^2;
+    e = sum(abs(Y(2:(Nfft2+1)) .* ...
+                (1 - lambda * Hcos2 ./ ...
+                 (Hcos1 + lambda * Hcos2))).^2) * 2;
+    e = e + abs(Y(1))^2;
 end
 
-gcv = e / trace;
+gcv = e/trace^2;
 end
 
 end
